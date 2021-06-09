74c865280ca0562960631f1fb911a4bef702947f
Parada <paradafaii28@gmail.com>
2021-06-09 15:08:52 -0400

    changeing the label

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,19 +3,24 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = { term: '' }
 
+    //stop pages form auto submit 
+    //because form element default when you press enter, they going to submit
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+
   
     render () {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
-                        <label>Image Search</label>
+                        <label>Images Searching</label>
                         <input 
                             type="text" 
                             value={this.state.term} 
-                            onChange={(e) => this.setState({ term: e.target.value.toUpperCase() }) } 
+                            onChange={(e) => this.setState({ term: e.target.value }) } 
                         />
-                        {/* <input type="text" onChange={(event) => console.log(event.target.value)} /> */}
                     </div>
                 </form>
             </div>
