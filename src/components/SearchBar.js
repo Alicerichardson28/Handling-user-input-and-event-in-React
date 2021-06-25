605c4c447f46539d4f2fbf623406ea338da3f2eb
Parada <paradafaii28@gmail.com>
2021-06-25 10:27:16 -0400

    fixing error can not find property state of indefined

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,15 +5,28 @@ class SearchBar extends React.Component {
 
     //stop pages form auto submit 
     //because form element default when you press enter, they going to submit
-    onFormSubmit(event) {
+
+    //fixing error can't read property 'state' of undefined
+    // onFormSubmit = (event) => {
+    //     event.preventDefault();
+        
+    //     console.log(this.state.term)
+    // }
+    onFormSubmit (event){
         event.preventDefault();
+        
+        console.log(this.state.term)
     }
 
   
     render () {
         return (
             <div className="ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
+                {/* make sure invoke onFormSubmit then will passing 
+                arrow function and pass down to the onFormSubmit,
+                 when form get submit will call arrow function, 
+                 then will fix the 'this problem of undefined*/}
+                <form onSubmit={(event) => this.onFormSubmit(event)} className="ui form">
                     <div className="field">
                         <label>Images Searching</label>
                         <input 
