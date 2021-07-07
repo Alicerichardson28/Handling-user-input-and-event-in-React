0754a5eaf280c620a6d3be31e0de4972b8d55ca7
Parada <paradafaii28@gmail.com>
2021-07-07 14:17:14 -0400

    fixing error

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,23 @@ import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
-    async onSearchSubmit(term) {
+  state = {
+    images: []
+  }
+    onSearchSubmit = async (term) => {
      const response = await axios.get("https://api.unsplash.com/search/photos", {
         params: { query: term },
         headers: {
           Authorization: `Client-ID ${process.env.REACT_APP_MY_API_KEY}`,
         },
       })
-      console.log(response.data.results)
+      this.setState({ images: response.data.results})
     }
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                Founds: {this.state.images.length} images
             </div>
         )
     } 
