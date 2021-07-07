8e40c0a221322adaaf2c5a9d5c91574386ce16ef
Parada <paradafaii28@gmail.com>
2021-07-07 14:28:04 -0400

    create api floder for refracter cod, then import into app.js

---
+++ src/api/unsplash.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+//create is allow you to create customize 
+//pre customize axios, and then import to app.js, cause we don't want this axios in app .js
+//the purpose for more clean code 
+
+export default axios.create({
+    baseURL: "https://api.unsplash.com",
+    headers: {
+          Authorization: `Client-ID ${process.env.REACT_APP_MY_API_KEY}`,
+        }
+});
